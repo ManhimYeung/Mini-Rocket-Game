f5028f88859bd7409e526011eb123dfe9438b2ac
Manhim Yeung <14059614@students.southwales.ac.uk>
2021-05-02 18:25:32 +0100

    adjusted movement, implemented explosion and level complete sound and particles

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,31 +1,77 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    AudioSource audioSource;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip successSound;
+
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem crashParticles;
+
+    float delayTimer = 1f;
+
+    bool isTransitioning = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter(Collision collision)
     {
+        if (isTransitioning) return;
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log("This thing is friendly. ");
+                //Debug.Log("This thing is friendly. ");
                 break;
             case "Finish":
-                Debug.Log("You completed the level. ");
+                StartSuccessSequence();
                 break;
             case "Fuel":
-                Debug.Log("You picked up some fuel. ");
+                //Debug.Log("You picked up some fuel. ");
                 break;
             default:
-                Debug.Log("You blew up. ");
+                StartCrashSequence();
                 break;
         }
-
-        string no = "false";
-        if (true)
-            DisplayMessage(no);
+    
     }
-    void DisplayMessage(string message)
+    void StartCrashSequence()
     {
-        Debug.Log(message);
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crashSound);
+        crashParticles.Play();
+        GetComponent<RocketMovement>().enabled = false;
+        Invoke("ReloadLevel", delayTimer);
     }
+    void StartSuccessSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        audioSource.PlayOneShot(successSound);
+        successParticles.Play();
+        GetComponent<RocketMovement>().enabled = false;
+        Invoke("LoadNextLevel", delayTimer);
+    }
+ 
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Load level 1 if last scene is finished.
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            nextSceneIndex = 0;
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
 }
--- Assets/Scripts/RocketMovement.cs
+++ Assets/Scripts/RocketMovement.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class RocketMovement : MonoBehaviour
 {
-    Rigidbody rb;
-    AudioSource audioSource;
-
-    /// <summary>
-    /// rocket stats such as flying speed and rotation speed
-    /// </summary>
+    // rocket parameters and stats
     float mainThrust = 1000.0f;
     float rotationThrust = 100.0f;
+    bool isFlying = false;
+
+    Rigidbody rb;
+    AudioSource audioSource;
+    [SerializeField] AudioClip mainEngine;
+
+    [SerializeField] ParticleSystem leftThrusterParticles;
+    [SerializeField] ParticleSystem rightThrusterParticles;
+    [SerializeField] ParticleSystem mainEngineParticles;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         audioSource = gameObject.GetComponent<AudioSource>();
+        Debug.Log(mainEngineParticles.isPlaying);
     }
 
     // Update is called once per frame
@@ -28,27 +34,43 @@ public class RocketMovement : MonoBehaviour
 
     void ProcessThrust()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
         {
-            //Debug.Log("Press space - thrusting");
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
             if (!audioSource.isPlaying)
-                audioSource.Play();
+                audioSource.PlayOneShot(mainEngine);
+
+            if (!mainEngineParticles.isPlaying) { }
+                mainEngineParticles.Play();
         }
         else
+        {
             audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+
     }
     void ProcessRotation()
     {
+        // Making sure the rocket is not able to rotate before leaving the launching pad.
+        if (!isFlying) return;
+
         if (Input.GetKey(KeyCode.A))
         {
-            //Debug.Log("Rotating left. ");
             ApplyRotation(rotationThrust);
+            if (!rightThrusterParticles.isPlaying)
+                rightThrusterParticles.Play();
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            //Debug.Log("Rotating right. ");
             ApplyRotation(-rotationThrust);
+            if (!leftThrusterParticles.isPlaying)
+            leftThrusterParticles.Play();
+        }
+        else
+        {
+            leftThrusterParticles.Stop();
+            rightThrusterParticles.Stop();
         }
     }
     void ApplyRotation(float rotationThisFrame)
@@ -56,5 +78,11 @@ public class RocketMovement : MonoBehaviour
         rb.freezeRotation = true; //freezing rotation so we can manually rotate.
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         rb.freezeRotation = false; //unfreezing rotation so the physics system can take over. 
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Friendly")
+            isFlying = true;
     }
 }
